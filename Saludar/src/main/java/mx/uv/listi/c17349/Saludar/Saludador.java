2664eb06bf696ae5252599c5381c04a6b24bf8ef
ChristianGLuna <zS23014138@estudiantes.uv.mx>
2025-11-04 17:24:37 -0600

    Actualización

---
+++ Saludar/src/main/java/mx/uv/listi/c17349/Saludar/Saludador.java
@@ -0,0 +1,21 @@
+package mx.uv.listi.c17349.Saludar;
+
+public class Saludador {
+    private String contenido;
+
+    public Saludador(){
+
+    }
+
+    public Saludador(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+}
